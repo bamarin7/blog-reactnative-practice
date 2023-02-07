a7315f8a5526e62c5de29db17aaed2fd9bd2f73e
Brian Marin Silva <marinbrian.as@gmail.com>
2023-02-07 14:56:22 -0600

    Adding required Context and ability to render a list of posts, as well as State in Context

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,9 +1,20 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, FlatList } from "react-native";
+import React, { useContext } from "react";
+import BlogContext from "../context/BlogContext";
 
 const IndexScreen = () => {
+  const blogPosts = useContext(BlogContext);
+
   return (
     <View>
       <Text>Index Screen</Text>
+      <FlatList
+        data={blogPosts}
+        keyExtractor={(blogPost) => blogPost.title}
+        renderItem={({ item }) => {
+          return <Text>{item.title}</Text>
+        }}
+      />
     </View>
   );
 };
