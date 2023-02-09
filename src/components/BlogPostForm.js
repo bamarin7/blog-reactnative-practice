0ab852f5532fec5feae9bbd67410c3bf8c3a251a
Brian Marin Silva <marinbrian.as@gmail.com>
2023-02-09 16:53:48 -0600

    Added backwards navigation and a reducer

--- src/components/BlogPostForm.js
+++ src/components/BlogPostForm.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
 
-const BlogPostForm = () => {
-  const [title, setTitle] = useState('');
-  const [content, setContent] = useState('');
+const BlogPostForm = ({ onSubmit, initialValues }) => {
+  const [title, setTitle] = useState(initialValues.title);
+  const [content, setContent] = useState(initialValues.content);
 
   return (
     <View>
@@ -21,10 +21,17 @@ const BlogPostForm = () => {
       />
       <Button
         title='Save Blog Post'
-        onPress={() => {}}
+        onPress={() => onSubmit(title, content)}
       />
     </View>
   );
+};
+
+BlogPostForm.defaultProps = {
+  initialValues: {
+    title: '',
+    content: ''
+  }
 };
 
 const styles = StyleSheet.create({
